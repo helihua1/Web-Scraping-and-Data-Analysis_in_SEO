74969a56347169953ac30cabf93f5657f05b4118
ZHANG\zhang <zhangbowendai@163.com>
2025-11-05 15:34:35 +0800

    no message

--- 多重占位符替换生成所有可能的结果/Generate_according_template.py
+++ 多重占位符替换生成所有可能的结果/Generate_according_template.py
@@ -28,14 +28,34 @@ for template in templates:
     # 找出模版中所有占位符
     placeholders = [ph for ph in replace_dict.keys() if ph in template]
 
+
     if not placeholders:
         results.append((template, template))  # 没有占位符，替换后与原模版相同
         continue
-
+    # replace_dict[ph]是list ，所以结果是[[],[],...]
     lists_to_product = [replace_dict[ph] for ph in placeholders]
 
+    # product(*lists_to_product) 会生成 所有可能组合（笛卡尔积）：
+    # 比如，3个list：地区list，等级，疾病
+    # ('济南', '三甲', '白癜风')
+    # ('济南', '三甲', '银屑病')
+    # ('济南', '二甲', '白癜风')
+    # ('济南', '二甲', '银屑病')
+    # ('石家庄', '三甲', '白癜风')
+    # ('石家庄', '三甲', '银屑病')
+    # ('石家庄', '二甲', '白癜风')
+    # ('石家庄', '二甲', '银屑病')
     for combo in product(*lists_to_product):
         temp = template
+        # zip() 是 Python 内置函数，用于 把多个可迭代对象按顺序“打包”成元组。当任意一个可迭代对象耗尽时，zip 就停止。
+        # a = [1, 2, 3]
+        # b = ['a', 'b', 'c']
+        # for x, y in zip(a, b):
+        #     print(x, y)
+        # 1 a
+        # 2 b
+        # 3 c
+        # for ph, val in ... → 循环解包元组，每次取一个占位符和对应的值。
         for ph, val in zip(placeholders, combo):
             temp = temp.replace(ph, val)
         results.append((temp, template))  # 保存替换后的句子和原模版
